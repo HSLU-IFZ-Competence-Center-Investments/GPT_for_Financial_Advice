c36d114523af8a5aca8d26cc9bc14d1fe529fd87
Ege Yilmaz <47591838+Quantumyilmaz@users.noreply.github.com>
2023-04-14 07:33:18 +0200

    New features in ChatSession
    Added capability to inject lines into chat history. Improved logging. Other.

--- utils.py
+++ utils.py
@@ -29,29 +29,26 @@ class ChatSession:
     
     def chat(self,user_input:Optional[dict|str]=None,*args,**kwargs):
         """ Say something to the model and get a reply. """
-        completion_index = 0 if kwargs.get('logprobs',False) else 1
-
-        if user_input is None:
-            user_input = input("> ")
         
-        if isinstance(user_input,dict):
-            assert completion_index, "Dictionary input style only supported for ChatCompletion and not Completion."
-            self.messages.append(user_input)
-            message = self.messages
-        else:
-            self.messages.append({"role": "user", "content": user_input})
-            message = self.messages if completion_index else user_input
-
+        completion_index = 0 if kwargs.get('logprobs',False) else 1
+        
         completion = self.completions[completion_index]
-        kwargs.update({completion['prompt']:message,'model':completion['model']})
 
-        choice = completion['completion'].create(*args,**kwargs).choices[0]
-        self.history.append(choice)
+        user_input = self.__get_input(user_input=user_input,log=True)
+        user_input = self.messages if completion_index else self.messages[-1]['content']
+        
+        kwargs.update({completion['prompt']:user_input,\
+                       'model':completion['model']})
+        
+        self.__get_reply(completion=completion['completion'],log=True,*args,**kwargs)
+
         self.history[-1].update({'completion_index':completion_index})
-        self.messages.append({"role": "assistant", "content": attrgetter(completion['text'])(choice)})
+
+        self.__call__(1)
     
     def display_probas(self,reply_index):
         """ Display probabilities of each word for the given reply by the model. """
+        
         history = self.history[reply_index]
         assert not history.completion_index
         probas = history.logprobs.top_logprobs
@@ -60,14 +57,27 @@ class ChatSession:
                                 columns=[str(i),f'{i}_proba'], \
                                     index=[f'candidate_{j}' for j in range(len(probas[0]))] \
                             ) for i,k in enumerate(probas)],axis=1).T
+    
+    def inject(self,line,role):
+        """ Inject lines into the chat. """
+
+        self.__log(message={"role": role, "content": line})
+
+
+    def clear(self):
+        """ Clears session. """
+
+        self.__init__()
 
     def save(self,filename):
         """ Saves the session to file. """
+
         with open(filename, 'wb') as f:
             pickle.dump(self, f)
 
     def load(self,filename):
         """ Loads up the session. """
+
         with open(filename, 'rb') as f:
             temp = pickle.load(f)
             self.messages = temp.messages
@@ -75,14 +85,45 @@ class ChatSession:
     
     def merge(self,filename):
         """ Merges another session from file with this one. """
+
         with open(filename, 'rb') as f:
             temp = pickle.load(f)
             self.messages += temp.messages
             self.history += temp.history
 
-    def __call__(self):
-        """ Display full chat log. """
-        for i,msg in enumerate(self.messages):
+    def __get_input(self,user_input,log:bool=False):
+        """ Converts user input to desired format. """
+
+        if user_input is None:
+            user_input = input("> ")
+        if not isinstance(user_input,dict): 
+            user_input = {"role": 'user', "content": user_input}
+        if log:
+            self.__log(user_input)
+        return user_input
+        
+    def __get_reply(self,completion,log:bool=False,*args,**kwargs):
+        """ Calls the model. """
+
+        reply = completion.create(*args,**kwargs).choices[0]
+        if log:
+            if hasattr(reply,'message'):
+                self.__log(message=reply.message,history=reply)
+            else:
+                self.__log(message={"role": 'assistant', "content": reply.text},history=reply)
+        return reply
+    
+    def __log(self,message:dict,history=None):
+        self.messages.append(message)
+        if history is not None:
+            assert isinstance(history,dict)
+            self.history.append(history)
+
+    def __call__(self,k:Optional[int]=None):
+        """ Display full chat log or last k messages. """
+
+        k = len(self.messages) if k is None else k
+        for i,msg in enumerate(self.messages[-k:]):
             message = msg['content']
             who = {'user':'User: ','assistant':'GPT: '}[msg['role']]
             print(who + message.strip() + '\n')
